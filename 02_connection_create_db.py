2bf0a26d1ef021a34d28e2927b6bf963cbcc26b5
prokofiew <filipmaciborski@gmail.com>
2023-11-27 18:16:10 +0100

    Connecting to db and creating db. Creating table.

---
+++ 02_connection_create_db.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+# creating database in memory
+# connection_1 = sqlite3.connect(':memory:')
+
+# by making a connection it will create database
+connection = sqlite3.connect('customer.db')
+
+
+
---
+++ 03_make_table.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+# creating database in memory
+# connection_1 = sqlite3.connect(':memory:')
+
+# by making a connection it will create database
+connection = sqlite3.connect('customer.db')
+
+# to create table need to use cursor 
+cursor = connection.cursor()
+
+# create a table
+# DATA TYPES: NULL, INTEGER, REAL(FLOAT), TEXR, BLOB(PICTURE)
+cursor.execute(""" CREATE TABLE customers (
+		first_name TEXT(20),
+		last_name TEXT(20),
+		email TEXT(50)
+	)
+""")
+# commit out command
+connection.commit()
+
+# closing connection
+connection.close()
+
+
