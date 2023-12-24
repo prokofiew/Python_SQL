0c5e5c2c26d87b3172f74d8f6d9751d4365bd09c
prokofiew <filipmaciborski@gmail.com>
2023-12-24 13:49:08 +0100

    displaying primary key as a rowid

---
+++ 08_primary_key.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+connection = sqlite3.connect('customer.db')
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT rowid, * FROM customers")
+
+items = cursor.fetchall()
+
+# displaying content with primary KEY - item[0] will be rowid 
+for item in items:
+	print(f"{item[0]}. {item[1]} {item[2]} {item[3]}")
+
+
+
+
+
+connection.commit()
+connection.close()
